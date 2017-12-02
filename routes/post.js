e5e344c4b660bbb49a3d3ac190f4dcbcf0090033
tetrapod117 <tetrapod117@gmail.com>
2017-12-02 12:24:09 +0900

    posgre用のモジュールを追加

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   res.redirect(302, "..");
-  res.send(req.body);
+  db.one("SELECT $1 AS value", 123)
+  .then(function (data) {
+      console.log("DATA:", data.value);
+  })
+  .catch(function (error) {
+      console.log("ERROR:", error);
+  });
+
+  //res.send(req.body);
 });
 
 module.exports = router;
